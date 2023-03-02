47f2d28b746b5063cdc154fc58247c321eb99425
Johnexzy <obajohn75@gmail.com>
2023-03-02 23:43:04 +0100

    first commit

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -45,6 +45,16 @@ export default function BlogPost({ post }: { post: any }) {
           </div>
         </div>
       </div>
+      <div className=" bg-blend-normal blog-header py-24 sm:py-32 border-t-8 border-gray-300">
+          <div className="flex justify-between mx-auto max-w-7xl px-6 lg:px-">
+            <h2 className="text-3xl font-medium tracking-tight text-gray-100 sm:text-4xl">
+              Startuplist Africa Blog
+            </h2>
+            <p className="mt-5 text-lg leading-8 text-gray-100">
+              &copy; 2023 All rights reserved. <a href="https://startuplist.africa" className=" decoration-gray-600">Startuplist Africa</a>
+            </p>
+          </div>
+        </div>
     </>
   );
 }
